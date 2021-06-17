84f0ae3664f4f3774ce696a200ce986b113b74e4
Thomas13702 <thomaspritchard137@btinternet.com>
2021-06-17 20:09:16 +0100

    Added sort post by date

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,27 @@
 import fs from "fs";
 import path from "path";
+import Link from "next/link";
 import Layout from "../components/Layout";
 import matter from "gray-matter";
+import Post from "../components/Post";
+import { sortByDate } from "../utils";
 
 export default function HomePage({ posts }) {
   console.log(posts);
   return (
     <Layout>
-      <h1>Hello World</h1>
+      <h1 className="text-5xl border-b-4 p-5 font-bold">Latests Posts</h1>
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
+        {posts.map((post, index) => (
+          <Post post={post} key={index} />
+        ))}
+      </div>
+
+      <Link href="/blog">
+        <a className="block text-center border border-gray-500 text-gray-800 rounded-md py-4 my-5 transition duration-500 ease select-none hover:text-white hover:bg-gray-900 focus:outline-none focus:shadow-outline w-full">
+          All Posts
+        </a>
+      </Link>
     </Layout>
   );
 }
@@ -33,7 +47,7 @@ export async function getStaticProps() {
 
   return {
     props: {
-      posts,
+      posts: posts.sort(sortByDate).slice(0, 6),
     },
   };
 }
